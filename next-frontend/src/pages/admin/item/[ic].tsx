ce2f20d5b99fe76b5b6c15f9e1ae8b99671f51b0
cancan007 <shota.moue0211@gmail.com>
2022-10-27 13:17:08 +0900

    showed multi images on admin/ic page

--- next-frontend/src/pages/admin/item/[ic].tsx
+++ next-frontend/src/pages/admin/item/[ic].tsx
@@ -5,17 +5,11 @@ import { getTxHistoryByIcSearchResult, useAPIGetTxHistoryByIc } from "src/hooks/
 import { ItemOrganizedType } from "src/hooks/api/admin/useAPIGetAllitems"
 
 
-
-
-
-
-
 const ItemDetail = () =>{
 
     const router = useRouter()
     const {ic, ownerId, owner, title, description, image, images} = router.query
     const {data:gettingTxHistory, refetch} = useAPIGetTxHistoryByIc({ic})
-    console.log(gettingTxHistory)
 
     useLayoutEffect(()=>{
       refetch()
@@ -23,8 +17,20 @@ const ItemDetail = () =>{
 
     return (
       <Box className="flex flex-col w-screen items-center text-black">
-        <Box className="flex flex-row justify-center gap-x-5">
-          <Image src={image ? image : images ? images[0] : ''} className="h-[500px] w-auto object-cover"/>
+        <Box className="flex flex-col lg:flex-row justify-center items-center gap-x-5 p-10">
+        {
+                                    image && !images ? (<Image src={image} className="h-[500px] w-auto object-cover"/>)
+                                    : !image && images ? (
+                                        <Box className="grid grid-cols-3 lg:grid-cols-3 gap-1">
+                                        {
+                                            images.map((im:string, i:number) => (
+                                                <Image key={i} src={im} className="object-cover  w-[200px] aspect-[4/3]"/>
+                                            ))
+                                        }
+                                        </Box>
+                                    )
+                                    : <></>
+                                }
           <Box className="flex flex-col items-center justify-center gap-y-2">
                 <p className="text-xl">{title}</p>
                 <p className="text-sm">description:{description}</p>
